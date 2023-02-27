655537f2aa94519cfe1cb4a4ba3d8c9fd8f48a2d
ngthvan1612 <ngthvan1612@gmail.com>
2023-02-27 15:32:51 +0700

    Initial

---
+++ src/main/java/com/hcmute/project/instagram/backend/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.hcmute.project.instagram.backend;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(InstagramBackendApplication.class);
+    }
+
+}
